95d744782426fb1ff28e4d9f442d58e9061a3f30
danikarasawa <danikarasawa@gmail.com>
2019-12-10 21:55:55 -0300

    new server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const app = require("./src/app");
-const port = process.env.port || 3001; //PROCESS.ENV.PORT é padrão do Heroku
+const PORT = process.env.PORT || 3001; //PROCESS.ENV.PORT é padrão do Heroku
 
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log('Servidor não encontrado. Corrige, amiga!');
   } else {
-    console.log(`Tá um tudo na porta ${port} ♥`)
+    console.log(`Tá um tudo na porta ${PORT} ♥`)
   };
 });
